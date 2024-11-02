37657dd83ad5aba7400f04d88beac7602a4c3935
YonteMoon <monteyoon1998@gmail.com>
2024-11-02 13:25:05 -0700

    added awaited type

---
+++ src/vault/ts/awaited.ts
@@ -0,0 +1,20 @@
+/**
+ * ? November 2nd, 2024
+ *
+ * * Removes the promise from any promise type.
+ * * From what I can see for use cases, great for helper functions that are
+ * * used in async functions
+ */
+
+async function doSomething() {
+  // Does something like await fetch(url)
+  return {
+    name: "Monte",
+    age: 26,
+  };
+}
+
+// Awaited removes the promise
+function useSomething(personInfo: Awaited<ReturnType<typeof doSomething>>) {
+  console.log(personInfo);
+}
