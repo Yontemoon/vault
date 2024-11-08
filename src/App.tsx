2f692fd44a16e27f1751bad48df843b5b1979dcb
YonteMoon <monteyoon1998@gmail.com>
2024-11-08 08:25:58 -0800

    adding sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "@/index.css";
 
+
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,33 @@
 import HeaderNavBar from "@/components/HeaderNavBar";
 import { Card } from "@/components/ui/card";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet, useRouterState } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <HeaderNavBar />
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
+  component: Root,
 });
+
+
+function Root () {
+  const router = useRouterState()
+  console.log(router);
+  const paths = router.location.pathname.split("/")
+  console.log(paths);
+
+  return (
+    <>
+    <SidebarProvider>
+      <HeaderNavBar />
+      <main>
+      {paths.length - 1 > 1 && <SidebarTrigger/>}
+        <Outlet />
+
+      </main>
+      <TanStackRouterDevtools />
+    </SidebarProvider>
+      
+    </>
+  )
+}
