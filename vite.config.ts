664477fd6d0d3ffdb2920cbd33895cfc87bc7889
YonteMoon <monteyoon1998@gmail.com>
2024-11-07 22:04:20 -0800

    Added tailwind/shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
     viteReact(),
     // ...,
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
