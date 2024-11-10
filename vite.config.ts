e7c19770afbe029607571571f111a1f6c84a06e1
YonteMoon <monteyoon1998@gmail.com>
2024-11-10 00:50:33 -0800

    removed outdir

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
   plugins: [
     TanStackRouterVite(),
     viteReact(),
