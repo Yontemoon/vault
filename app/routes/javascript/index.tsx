374c5107e47c6e76197e07071662efd0c9cdd60b
YonteMoon <monteyoon1998@gmail.com>
2024-11-10 23:38:58 -0800

    added small intro to JS

--- app/routes/javascript/index.tsx
+++ app/routes/javascript/index.tsx
@@ -10,6 +10,12 @@ function RouteComponent() {
     <div>
       <Heading>Javascript</Heading>
       <p>Ahhh... Vanilla Javascript. Don't think I forgot about you!</p>
+      <br/>
+      <p>
+        Writing Javascript without type safety now feels like walking outside naked. I just feel exposed. 
+        While i rarely use Vanilla JS, this section will be focused on the finer details of JS. There is so much to learn, and I 
+        accepted that I won't ever learn everything to learn. But I'll give it a try with this website. 
+      </p>
     </div>
   );
 }
