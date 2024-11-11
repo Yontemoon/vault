41a123e6d81d3e6dd52ade4f5291fae78214387f
YonteMoon <monteyoon1998@gmail.com>
2024-11-11 14:47:28 -0800

    Testing out fiel path

--- app/server/file.ts
+++ app/server/file.ts
@@ -1,14 +1,19 @@
 import { createServerFn } from "@tanstack/start";
-import fs from "node:fs/promises";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const getFile = createServerFn("GET", async (path: string) => {
+const __filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(__filename);
+
+const getFile = createServerFn("GET", async (pathUrl: string) => {
   try {
-    const data = await fs.readFile(`./app/vaults${path}`, {
-      encoding: "utf8",
-    });
+    const filePath = path.resolve(dirname, `../vaults${pathUrl}`);
+    console.log(filePath);
+    const data = await fs.readFile(filePath, "utf-8");
     return data;
   } catch (err) {
-    console.error(err);
+    console.error("File not found or another error:", err);
     return "Something went wrong";
   }
 });
