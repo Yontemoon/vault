067db8ff622af9aad0af0392d120f30ec2d77f1e
YonteMoon <monteyoon1998@gmail.com>
2024-11-10 23:43:45 -0800

    started react sec

--- app/routes/react/index.tsx
+++ app/routes/react/index.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Heading from "@/components/heading";
 
 export const Route = createFileRoute("/react/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return "Hello /react/!";
+  return (
+    <div>
+      <Heading as="h1" size="xl">React</Heading>
+      <Heading as="h2" size="sm">The library that feels like a framework.</Heading>
+    </div>
+  )
 }
