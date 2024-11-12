01c4711642d384d0da7faf8d7bee426e3b5055ac
YonteMoon <monteyoon1998@gmail.com>
2024-11-11 21:56:24 -0800

    testing fs promise

--- app/server/shiki.ts
+++ app/server/shiki.ts
@@ -1,14 +1,14 @@
 import { createServerFn } from "@tanstack/start";
 import { codeToHtml } from "shiki";
 import path from "path";
-import fs from "fs";
+import { promises as fs } from "fs";
 
 const getShiki = createServerFn("GET", async (codeStr: string) => {
   try {
     // const content = await fs.readFile(`./app/vaults${codeStr}`, "utf8");
     const url = path.join(process.cwd(), `/app/vaults${codeStr}`);
 
-    const content = fs.readFileSync(url, "utf8");
+    const content = await fs.readFile(url, "utf8");
 
     const html = await codeToHtml(content, {
       lang: "typescript",
