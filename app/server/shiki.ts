9e12cbbe6062c3c8b351dd4ef0e56dbce6d5c1e0
YonteMoon <monteyoon1998@gmail.com>
2024-11-12 12:27:00 -0800

    testing readFile

--- app/server/shiki.ts
+++ app/server/shiki.ts
@@ -1,7 +1,7 @@
 import { createServerFn } from "@tanstack/start";
 import { codeToHtml } from "shiki";
 import path from "path";
-import fs from "fs";
+import fs from "fs/promises";
 import { awaited, checkTypes, constReadOnly } from "@/vaults";
 
 // const loadFile = async (fileName: string) => {
@@ -18,18 +18,18 @@ console.log(awaited, checkTypes, constReadOnly);
 
 const getShiki = createServerFn("GET", async (codeStr: string) => {
   try {
-    // const content = await fs.readFile(`./app/vaults${codeStr}`, "utf8");
+    const content = await fs.readFile(`./app/vaults${codeStr}`, "utf8");
     // const url = path.join(process.cwd(), `/app/vaults${codeStr}`);
-    console.log("Current working directory:", process.cwd());
-    const dataPath = path.resolve(
-      process.cwd(),
-      "app",
-      "vaults",
-      "ts",
-      "awaited.ts"
-    );
+    // console.log("Current working directory:", process.cwd());
+    // const dataPath = path.resolve(
+    //   process.cwd(),
+    //   "app",
+    //   "vaults",
+    //   "ts",
+    //   "awaited.ts"
+    // );
 
-    const content = fs.readFileSync(dataPath, "utf-8");
+    // const content = fs.readFileSync(dataPath, "utf-8");
     // const content = await fs.readFile(url, "utf8");
 
     const html = await codeToHtml(content, {
