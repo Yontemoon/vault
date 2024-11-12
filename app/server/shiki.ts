43c2cb99c2d5fba50a22f39167aa6db87c3e473c
YonteMoon <monteyoon1998@gmail.com>
2024-11-11 23:25:33 -0800

    added var and task in url

--- app/server/shiki.ts
+++ app/server/shiki.ts
@@ -2,21 +2,33 @@ import { createServerFn } from "@tanstack/start";
 import { codeToHtml } from "shiki";
 import path from "path";
 import fs from "fs";
-import { c } from "node_modules/vite/dist/node/types.d-aGj9QkWt";
 
 const getShiki = createServerFn("GET", async (codeStr: string) => {
-  "use server";
   try {
     // const content = await fs.readFile(`./app/vaults${codeStr}`, "utf8");
     // const url = path.join(process.cwd(), `/app/vaults${codeStr}`);
-    const dataPath = path.resolve(
-      process.cwd(),
-      "app",
-      "vaults",
-      "ts",
-      "awaited.ts"
-    );
+    let dataPath = "";
+
     console.log(dataPath);
+    if (import.meta.env.MODE === "development") {
+      dataPath = path.resolve(
+        process.cwd(),
+        "app",
+        "vaults",
+        "ts",
+        "awaited.ts"
+      );
+    } else {
+      dataPath = path.resolve(
+        process.cwd(),
+        "var",
+        "task",
+        "app",
+        "vaults",
+        "ts",
+        "awaited.ts"
+      );
+    }
     const content = fs.readFileSync(dataPath, "utf-8");
     // const content = await fs.readFile(url, "utf8");
 
