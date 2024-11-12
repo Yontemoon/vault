98be1a0167ef61ea5523d14ec08a024916f1766e
YonteMoon <monteyoon1998@gmail.com>
2024-11-11 21:49:17 -0800

    testing "process.cwd"

--- app/server/shiki.ts
+++ app/server/shiki.ts
@@ -1,10 +1,15 @@
 import { createServerFn } from "@tanstack/start";
 import { codeToHtml } from "shiki";
-import fs from "fs/promises";
+import path from "path";
+import fs from "fs";
 
 const getShiki = createServerFn("GET", async (codeStr: string) => {
   try {
-    const content = await fs.readFile(`./app/vaults${codeStr}`, "utf8");
+    // const content = await fs.readFile(`./app/vaults${codeStr}`, "utf8");
+    const url = path.join(process.cwd(), `/app/vaults${codeStr}`);
+
+    const content = fs.readFileSync(url, "utf8");
+
     const html = await codeToHtml(content, {
       lang: "typescript",
       theme: "vitesse-dark",
