2cd73c9c29be8b33c0014d6ce5ffcbe21e7ed4fe
YonteMoon <monteyoon1998@gmail.com>
2024-11-12 22:05:16 -0800

    Changed url again!

--- app/server/shiki.ts
+++ app/server/shiki.ts
@@ -41,7 +41,7 @@ const getShiki = createServerFn("GET", async (codeStr: string) => {
     let dataPath = "";
 
     if (import.meta.env.PROD) {
-      dataPath = path.resolve("output", "static", "awaited.ts");
+      dataPath = path.resolve("awaited.ts");
     } else {
       dataPath = path.resolve(process.cwd(), "public", "awaited.ts");
     }
