76b8fd277a992e0e6ce9cd32f0eb866f6998c5e0
YonteMoon <monteyoon1998@gmail.com>
2024-11-14 22:29:23 -0800

    footer link to external site

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,9 @@ import { Link } from "@tanstack/react-router";
 const Footer = () => {
   return (
     <footer className="flex justify-center items-center mt-10 h-14">
-      <Link to="/">Monte Yoon 2024</Link>
+      <a href="https://monteyoon.com" target="_blank" rel="noopener noreferrer">
+        Monte Yoon 2024
+      </a>
     </footer>
   );
 };
