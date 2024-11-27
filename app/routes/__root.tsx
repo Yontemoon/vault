16717b24f0b71dccaceef2b6cb6c0bf5d73f43e3
YonteMoon <monteyoon1998@gmail.com>
2024-11-27 11:29:28 -0800

    updated to use beta tanstack start

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -5,52 +5,39 @@ import {
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import HeaderNavBar from "../components/header-navbar";
-import { SidebarProvider } from "../components/ui/sidebar";
-import AppSidebar from "../components/app-sidebar";
 import Error from "../components/error";
-import useIsParent from "../hooks/use-is-parent";
 import Footer from "@/components/footer";
 import styles from "@/index.css?url";
-import { Body, Head, Html, Meta, Scripts } from "@tanstack/start";
+import { Meta, Scripts } from "@tanstack/start";
 import type { ReactNode } from "react";
 import React from "react";
 import { ThemeProvider } from "@/context/theme-provider";
 
 export const Route = createRootRoute({
-  meta: () => [
-    {
-      charSet: "utf-8",
-    },
-    {
-      name: "viewport",
-      content: "width=device-width, initial-scale=1",
-    },
-    {
-      title: "Monte's Vault",
-    },
-  ],
-  links: () => [
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap",
-    },
-    {
-      rel: "stylesheet",
-      href: styles,
-    },
-  ],
-  scripts: () =>
-    import.meta.env.DEV
-      ? [
-          {
-            type: "module",
-            children: `import RefreshRuntime from "/_build/@react-refresh";
-              RefreshRuntime.injectIntoGlobalHook(window)
-              window.$RefreshReg$ = () => {}
-              window.$RefreshSig$ = () => (type) => type`,
-          },
-        ]
-      : [],
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "Monte's Vault",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap",
+      },
+      {
+        rel: "stylesheet",
+        href: styles,
+      },
+    ],
+  }),
 
   component: RootComponent,
   errorComponent: Error,
@@ -61,21 +48,16 @@ export const Route = createRootRoute({
 });
 
 function RootComponent() {
-  // const isParent = useIsParent();
   return (
     <RootDocument>
       <ThemeProvider>
         <HeaderNavBar />
         <div className="mx-auto w-ful max-w-5xl">
-          {/* <SidebarProvider> */}
-          {/* {isParent && <AppSidebar />} */}
           <main className=" mx-5">
             <div className="w-full">
               <Outlet />
             </div>
           </main>
-          {/* </SidebarProvider> */}
-          {/* <ThemeToggle /> */}
           {import.meta.env.DEV && <TanStackRouterDevtools />}
         </div>
         <Footer />
@@ -86,15 +68,15 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <Html lang="en">
-      <Head>
+    <html lang="en">
+      <head>
         <Meta />
-      </Head>
-      <Body>
+      </head>
+      <body>
         <React.Suspense>{children}</React.Suspense>
         <ScrollRestoration />
         <Scripts />
-      </Body>
-    </Html>
+      </body>
+    </html>
   );
 }
